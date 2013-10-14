6b5f8ef04bec2b0c7735058154bf3c600e0f314c
Corey O'Connor <corey@ooyala.com>
2013-10-14 12:24:18 -0700

    send errors back as 400 responses

--- TinyHttp.hs
+++ TinyHttp.hs
@@ -19,20 +19,21 @@ import Network.Stream
 
 main = withSocketsDo $ do
     http_socket <- listenOn $ PortNumber 9090
-    dispatch_on_accept http_socket $ \byte_stream ->
-        either (\failure -> print failure)
-               (\r -> do
-                    in_body <- evaluate $! rqBody r
-                    respondHTTP byte_stream $ handle_request in_body
-               )
+    dispatch_on_accept http_socket $ either handle_failed_request handle_valid_request
     sClose http_socket
 
-handle_request in_body = Response (2,0,0) "OK" [mkHeader HdrConnection "close"] $
-    encodeUtf8 $ pack $ show in_body
+handle_failed_request failure = return $ Response (4,0,0) "Bad Request" [mkHeader HdrConnection "close"]
+                                                  (encodeUtf8 $ pack $ show failure)
 
-dispatch_on_accept :: Socket -> (HandleStream BS.ByteString -> Result (Request BS.ByteString) -> IO ()) -> IO ()
+handle_valid_request request = do
+    let request_body = rqBody request
+    return $ Response (2,0,0) "OK" [mkHeader HdrConnection "close"]
+                      (encodeUtf8 $ pack $ show request_body)
+
 dispatch_on_accept http_socket handler = forever $ accept http_socket >>= forkIO . httpHandler . fst
     where
         httpHandler client_socket = bracket (socketConnection "client" 0 client_socket)
                                             Network.HTTP.close
-                                            (\byte_stream -> receiveHTTP byte_stream >>= handler byte_stream)
+                                            client_interact
+        client_interact :: HandleStream BS.ByteString -> IO ()
+        client_interact byte_stream = receiveHTTP byte_stream >>= handler >>= respondHTTP byte_stream
